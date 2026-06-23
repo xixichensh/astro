554fb258febdfa64a1d16c4adbcde9c32e2e3140
陈峰 <chenfengxixi44@126.com>
2026-06-23 16:48:00 +0800

    Update Astro configuration with site and base URL settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
+/*
+ * @Author: cfxixi
+ * @Date: 2026-06-22 18:08:03
+ * @LastEditors: cfxixi
+ * @LastEditTime: 2026-06-23 16:47:43
+ * @FilePath: /astro/astro.config.mjs
+ * @Description: --
+ * 短号: 65030
+ * Copyright (c) 2026 by cfxixi/天天基金, All Rights Reserved.
+ */
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://xixichensh.github.io',
+  base: '/astro',
+});
